9b31c738ac81b2b20cf1a1a66c07de0f1006134b
Eduardo <cheligaedu@gmail.com>
2025-04-07 08:45:20 -0300

    tarefa

---
+++ tarefas/lista31b.js
@@ -0,0 +1,18 @@
+function quadrante(x,y){
+    if (x==0 && y==0) return "origem";
+    if (x>= 0 && y >= 0) return "Q1"
+    if (x<0 && y <0) return "Q3"
+    if (x>=0 && y <0) return "Q4"
+    return "Q2"
+}
+function triang (xx,yy,zz){
+    let array = [xx,yy,zz]
+    array.sort(function(a,b) {return b-a});
+    let x = array[0]; let y = array[1]; let z = array[2]
+    if (x >= y+z) return console.log("nao forma triangulo")
+    if (Math.pow(x,2) == Math.pow(y,2)+Math.pow(z,2)) console.log("triangulo retangulo")
+    if (Math.pow(x,2) > Math.pow(y,2)+Math.pow(z,2)) console.log("triangulo obtusangulo")
+    if (Math.pow(x,2) < Math.pow(y,2)+Math.pow(z,2)) console.log("triangulo acutangulo")
+    if (x==y && x==z) return console.log("triangulo equilatero")
+    if (x==y || x==z || z==y) console.log("triangulo isoceles")
+}
