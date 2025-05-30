df6978c34493889f046c1af5957e7ae804881515
Eduardo <cheligaedu@gmail.com>
2025-05-30 10:03:46 -0300

    asdsg

---
+++ aulas/30/05/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './Header'
+import './App.css'
+
+function App() {
+  return(<div className='app'>
+    <Header />
+  </div>)
+}
+
+export default App
---
+++ aulas/30/05/src/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header(){
+    return(
+        <header className='app-header'>
+            <h1>Bem vindo chefe! Hoje será um dia com ótimos resultados!</h1>
+        </header>
+    );
+        
+}
+export default Header;
